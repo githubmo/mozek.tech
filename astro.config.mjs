ee20ca2d24911d5ae6acc8bf6e25f71f1a73cc56
Mohammed Abdulrazeg <github@mzawi.me>
2026-03-23 11:56:49 +0000

    Initial site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,14 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    build: {
+      rollupOptions: {
+        onwarn(warning, defaultHandler) {
+          if (warning.code === 'UNUSED_EXTERNAL_IMPORT' && warning.exporter === '@astrojs/internal-helpers/remote') return;
+          defaultHandler(warning);
+        },
+      },
+    },
   }
 });
